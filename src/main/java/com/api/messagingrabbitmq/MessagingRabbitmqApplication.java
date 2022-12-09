b018005611b0db815584061c191344d7f49a0cdd
andre-alck <andrealck1@gmail.com>
2022-12-09 16:07:43 -0300

    feat: add Messaging with RabbitMQ guide

--- src/main/java/com/api/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/api/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,13 +1,53 @@
 package com.api.messagingrabbitmq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
+	static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+
+	static final String QUEUE_NAME = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(QUEUE_NAME, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(TOPIC_EXCHANGE_NAME);
+	}
+
+	@Bean
+	Binding binding(org.springframework.amqp.core.Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(QUEUE_NAME);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingRabbitmqApplication.class, args);
 	}
-
 }
